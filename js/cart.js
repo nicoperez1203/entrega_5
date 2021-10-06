0bcfa1ad15cf0deaed8451c6ce86ee6796386c73
Nicolas Perez <nipefo1203@gmail.com>
2021-10-06 09:29:19 -0300

    entrega_5.1

--- js/cart.js
+++ js/cart.js
@@ -8,15 +8,15 @@ mostrarNomb()
 
 
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(CART_INFO_URL).then(function (resultObj) {
+    getJSONData(CARRO_2_ARTICULOS).then(function (resultObj) {
 
         if (resultObj.status === "ok") {
-           
+
             prodCarrito = resultObj.data;
             datosCarro = prodCarrito.articles;
             mostrarCarrito(datosCarro);
 
-            
+
         }
     });
 });
@@ -24,73 +24,81 @@ document.addEventListener("DOMContentLoaded", function (e) {
 var prodCarrito = [];
 
 
+
+
 function mostrarCarrito(array) {
 
     let data = "";
     for (let i = 0; i < array.length; i++) {
         let datoscarrito = array[i];
-        let total = datoscarrito.unitCost * datoscarrito.count;
+        let totalProducto = datoscarrito.unitCost * datoscarrito.count;
+        // let subTotal =+ subTotal + totalProducto;
         
+        if (datoscarrito.currency === "USD"){
+            totalProducto = totalProducto * 40;
+            }
+
+        // let subTotal = totalProducto + subTotal;
+        // document.getElementById("precioSubTotal").innerHTML = subTotal;
+
+         
+       
+
         {
 
             data +=
-            
-`
-                        <thead class="text-muted">
-                            <tr class="small text-uppercase">
-                                <th scope="col">Producto</th>
-                                <th scope="col" width="120">Unidades</th>
-                                <th scope="col" width="120">Sub-total</th>
-                                <th scope="col" class="text-right d-none d-md-block" width="200"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
+
+                `
+                        
+                        
                                 <tr>
                                     <td>
                                         <figure class="itemside align-items-center">
                                         <div class="aside"><img src="` + datoscarrito.src + `" class="img-sm"></div>
                                         <figcaption class="info"> <a href="#" class="title text-dark" data-abc="true">`+ datoscarrito.name + `</a>
-                                        <p class="text-muted small">Unidades: ` + datoscarrito.count + ` <br> Costo unitario: `+ datoscarrito.currency +` $` + datoscarrito.unitCost + `</p>
-                        
-                    </figcaption>
-                </figure>
-            </td>
-            <td> <small class="text-muted">Maximo 10 unidades</small> 
-            <input type="number" id="tentacles" name="tentacles"  min="1" max="10"> </td>
-            <td>
-                <div class="price-wrap"> <var class="price">UYU $` + total + `</var></div>
-            </td>
-            <td class="text-right d-none d-md-block"><a href="" class="btn btn-danger" data-abc="true"><i class="fa fa-trash"></i> Remover</a></td>
-        </tr>
+                                        <p class="text-muted small" name="subtotal" >Costo unitario: ` + datoscarrito.currency + ` ` + datoscarrito.unitCost + `</p>
+                                        </figcaption>
+                                        </figure>
+                                    </td>
+                                    <td> 
+                                        <input type=number value="` + datoscarrito.count + `">
+                                    </td>
+                                    <td>
+                                        <div class="price-wrap"> <var class="price">$` + totalProducto + `</var></div>
+                                    </td>
+                                    <td class="text-right d-none d-md-block"><a href="" class="btn btn-danger" data-abc="true"><i class="fa fa-trash"></i> Remover</a>
+                                    </td>
+                                </tr>
+                                
 
         
-        ` 
-        
-        /*`
-                    
-                   
-                    <div>
-                    <h5>`+ datoscarrito.name + `</h5> 
-                    <br>
-                        <div>
-                        <p>`+ datoscarrito.count + `</p>
-                        </div>
-                        <img src="` + datoscarrito.src + `" width="45" height="25" alt="`+ datoscarrito.name + `">
-                        
-                            
-                             <div>
-                              <small class="text-muted">Costo: ` + datoscarrito.unitCost + ` </small>
-                             </div>
-                        
-                    </div>
-                    <hr>
-                
-                `*/
+        `
         }
+    
 
-        document.getElementById("prueba").innerHTML = data;
+        document.getElementById("carrito").innerHTML = data;
+        // document.getElementById("precioSubTotal").innerHTML = subTotal;
+        
+        
     }
 }
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
 
 
 
