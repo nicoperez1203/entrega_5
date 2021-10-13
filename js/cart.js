8a7f40c3b5ac0264d7627cb2ac05ed711e549999
Nicolas Perez <nipefo1203@gmail.com>
2021-10-13 11:19:59 -0300

    entrega_5final

--- js/cart.js
+++ js/cart.js
@@ -15,9 +15,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
             prodCarrito = resultObj.data;
             datosCarro = prodCarrito.articles;
             mostrarCarrito(datosCarro);
-            
-
-
         }
     });
 });
@@ -27,12 +24,6 @@ var subTotal = 0;
 var subsub = 0;
 
 
-
-
-
-
-
-
 function mostrarCarrito(array) {
 
     let data = "";
@@ -40,57 +31,41 @@ function mostrarCarrito(array) {
         let datoscarrito = array[i];
         
         
-        
-        
         if (datoscarrito.currency === "USD"){
             datoscarrito.unitCost = datoscarrito.unitCost * 40;
             }
 
             let totalProducto = datoscarrito.unitCost * datoscarrito.count;
-        
-             subTotal += totalProducto;
-       
-        
-
-            {
+            subTotal += totalProducto;
+       {
 
             data +=
 
+                `   <tr>
+                        <td>
+                            <figure class="itemside align-items-center">
+                                <div class="aside"><img src="` + datoscarrito.src + `" class="img-sm"></div>
+                                    <figcaption class="info"> <a href="products.html" class="title" data-abc="true">`+ datoscarrito.name + `</a>
+                                    </figcaption>
+                            </figure>
+                        </td>
+                        <td> 
+                            <p class="precio"> ` + datoscarrito.unitCost + `</p>
+                        </td>
+                        <td> 
+                            <input type=number id="cant${i}" onchange="tiempoReal()" style="width: 45px" name="cantidades" min="1" value="` + datoscarrito.count + `">
+                        </td>
+                        <td> 
+                            <p id="subtProd${i}">` + totalProducto + `</p>
+                        </td>
+                        <td class="text-right d-none d-md-block"><a onclick="deleteRow(this)" class="btn btn-danger" data-abc="true"><i class="fa fa-trash"></i> Remover</a>
+                        </td>
+                    </tr>
                 `
-                        
-                        
-                                <tr>
-                                    <td>
-                                        <figure class="itemside align-items-center">
-                                        <div class="aside"><img src="` + datoscarrito.src + `" class="img-sm"></div>
-                                        <figcaption class="info"> <a href="products.html" class="title" data-abc="true">`+ datoscarrito.name + `</a>
-                                        
-                                        </figcaption>
-                                        </figure>
-                                    </td>
-                                    <td> 
-                                    <p class="precio"> ` + datoscarrito.unitCost + `</p>
-                                    </td>
-                                    
-                                    <td> 
-                                        <input type=number id="cant${i}" onchange="tiempoReal()" style="width: 45px" name="cantidades" min="0" value="` + datoscarrito.count + `">
-                                    </td>
-                                    
-                                    <td class="text-right d-none d-md-block"><a onclick="deleteRow(this)" class="btn btn-danger" data-abc="true"><i class="fa fa-trash"></i> Remover</a>
-                                    </td>
-                                </tr>
-                                
-
-        
-        `
         }
     
-        
-        
         document.getElementById("carrito").innerHTML = data;
         document.getElementById("precioSubTotal").innerHTML = subTotal;
-        
-        
         
     }
 }
@@ -99,46 +74,32 @@ function mostrarCarrito(array) {
 
 
   function standard(){
-    
-    
       document.getElementById("tipoEnvio").innerHTML = "Standard";
       valor = document.getElementById("precioSubTotal").innerHTML;
       nuevoValor = (valor * 5) / 100;
       document.getElementById("costoEnvio").innerHTML = `Costo de envío: $` +nuevoValor;
       precioFinal = parseFloat(valor) + nuevoValor;
       document.getElementById("precioFinal").innerHTML = `Total: $` +precioFinal;
-
-  }
+    }
     
 
-
-
-  function express(){
-
-    
+    function express(){
       document.getElementById("tipoEnvio").innerHTML = "Express";
       valor = document.getElementById("precioSubTotal").innerHTML;
       nuevoValor = (valor * 7) / 100;
       document.getElementById("costoEnvio").innerHTML = `Costo de envío: $` +nuevoValor;
       precioFinal = parseFloat(valor) + nuevoValor;
       document.getElementById("precioFinal").innerHTML = `Total: $` +precioFinal;
-    
-
-  }
+    }
 
   function premium(){
-
-    
       document.getElementById("tipoEnvio").innerHTML = "Premium";
       valor = document.getElementById("precioSubTotal").innerHTML;
       nuevoValor = (valor * 15) / 100;
       document.getElementById("costoEnvio").innerHTML = `Costo de envío: $` +nuevoValor;
       precioFinal = parseFloat(valor) + nuevoValor;
-      document.getElementById("precioFinal").innerHTML = `Total: $` +precioFinal;
-
-      
-
-  }
+      document.getElementById("precioFinal").innerHTML = `Total: $` +precioFinal;   
+     }
 
 
 
@@ -147,21 +108,35 @@ function tiempoReal(){
     let precios = document.getElementsByClassName("precio");
     let cantidades = document.getElementsByTagName("input");
     
+    
     subsub = 0;
     for(let i = 0; i < precios.length; i++){
-        
-        subsub += parseFloat(precios[i].innerHTML) * cantidades[i].value;
+            subsub += parseFloat(precios[i].innerHTML) * cantidades[i].value;
+            document.getElementById("subtProd"+i).innerHTML = parseFloat(precios[i].innerHTML) * cantidades[i].value;
         }
 
         document.getElementById("precioSubTotal").innerHTML= subsub;
+        
+        
+
+        let envio = document.getElementById("envio").value;
+       
+        if (envio == 1){
+            standard();
+        } if (envio == 2){
+                express();
+        }   if (envio == 3){
+                    premium();
+        }
+
 
 }
 
 function deleteRow(r)
     {
-    var fila = r.parentNode.parentNode.rowIndex
-    document.getElementById("tablaProductos").deleteRow(fila)
-}
+    var fila = r.parentNode.parentNode.rowIndex;
+    document.getElementById("tablaProductos").deleteRow(fila);
+    }
 
 
 
